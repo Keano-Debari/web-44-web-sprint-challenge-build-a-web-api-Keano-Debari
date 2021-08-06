1ed6f203c4a0dbe3c9b9b9ee2a2b5b974df7fc86
unknown <Keano@Firelink.Firelink>
2021-08-06 12:15:08 -0700

    mvp

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActionId(req, res, next) {
+    try {
+        const action = await Action.get(req.params.id)
+        if(action) {
+            req.action = action
+            next()
+        }
+        else {
+            next({status: 404, message: 'action not found'})
+        }
+    }
+    catch (err) {
+        res.status(500).json({message: 'cant find action'})
+    }
+}
+
+function validateAction(req, res, next) {
+    const { project_id, description, notes } = req.body
+    if(!project_id || !description || !notes) {
+        req.status(400).json({message: 'missing required fields'})
+    } 
+    else if (description > 128) {
+        next({ message: 'description cant be longer than 128 characters'})
+    }
+    else {
+        next()
+    }
+}
+
+
+module.exports = { validateActionId, validateAction }
